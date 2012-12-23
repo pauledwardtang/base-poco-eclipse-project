73e92fa95c369525891b06584c7de049797bfa75
pauledwardtang <pauledangerously1@gmail.com>
2012-12-23 02:12:48 -0500

    Fixed project dependency issues. The resultant program must be run with its shared libraries in the same dir. (Building via Eclipse will now copy over those shared libs)

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,37 @@
 
 #include <iostream>
+#include <fstream>
+
+#include "Poco/DOM/DOMParser.h"
+#include "Poco/DOM/Document.h"
+#include "Poco/DOM/NodeIterator.h"
+#include "Poco/DOM/NodeFilter.h"
+#include "Poco/DOM/AutoPtr.h"
+#include "Poco/SAX/InputSource.h"
 
 int main()
 {
     std::cout << "!!!Hello World!!!" << std::endl; // prints !!!Hello World!!!
+
+    try
+    {
+        std::ifstream in("resources/test.xml");
+        Poco::XML::InputSource src(in);
+        Poco::XML::DOMParser parser;
+        Poco::AutoPtr<Poco::XML::Document> pDoc = parser.parse(&src);
+
+        Poco::XML::NodeIterator it(pDoc, Poco::XML::NodeFilter::SHOW_ELEMENT);
+        Poco::XML::Node* pNode = it.nextNode();
+        while (pNode)
+        {
+         std::cout<<pNode->nodeName()<<":"<< pNode->nodeValue()<<std::endl;
+         pNode = it.nextNode();
+        }
+    }
+    catch(Poco::Exception& ex)
+    {
+        std::cout << ex.message();
+    }
+
     return 0;
 }
